3bbf5157de21675db85d8590d4f445bdcaf07d8f
AlejandroQL23 <ale23200@gmail.com>
2024-09-20 16:02:24 -0600

    Analisis employees data
    Analisis employees data in R

---
+++ AnalisisDatosEmpleos/api_Stats.R
@@ -0,0 +1,307 @@
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("rpart","aplpack","corrplot","sm","gapminder","openintro",
+              "dplyr","ggplot2","readr","lubridate","tidyr", "forecast",
+              "corrplot", "plotly","plumber")
+ipak(packages)
+
+#* @apiTitle API para leer archivo CSV, mostrar columnas y seleccionar una columna específica
+
+#* Leer el archivo CSV y mostrar las columnas
+#* @param file_path Ruta completa del archivo CSV
+#* @param column_name Nombre de la columna a mostrar
+#* @get /stats
+function(file_path, column_name) {
+  # Leer el archivo CSV
+  data <- read.csv(file_path)
+  
+  # Verificar si la columna existe en los nombres de las columnas del dataframe
+  if (!(column_name %in% colnames(data))) {
+    return(list(
+      error = TRUE,
+      message = paste("La columna", column_name, "es inválida. Intente con un nombre de columna correcto.")
+    ))
+  }
+
+  # Extraer la columna especificada
+  column_data <- data[[column_name]]
+  
+  if (!is.numeric(column_data)) {
+    return(list(
+      error = TRUE,
+      message = paste("La columna", column_name, "no es de tipo numérico. Por favor, seleccione una columna que contenga datos numéricos.")
+    ))
+  }
+  
+  # Calcular las estadísticas
+  stats <- list(
+    column_name = column_name,
+    min = min(column_data, na.rm = TRUE),
+    max = max(column_data, na.rm = TRUE),
+    mean = mean(column_data, na.rm = TRUE),
+    median = median(column_data, na.rm = TRUE),
+    quartiles = quantile(column_data, probs = c(0.25, 0.5, 0.75), na.rm = TRUE)
+  )
+  
+  stats
+}
+
+
+#* @apiTitle API para obtener la respuesta a la primera Hipotesis
+
+#* ¿Existe una diferencia significativa en el salario mensual entre empleados de diferentes géneros en la misma posición y departamento?
+#* @param file_path Ruta completa del archivo CSV
+#* @get /fHipo
+function(file_path) {
+  # Leer el archivo CSV
+  data <- read.csv(file_path)
+  
+  salary_summary <- data %>%
+    group_by(Gender, Department) %>%
+    summarise(Average_Salary = mean(MonthlySalary, na.rm = TRUE)) %>%
+    arrange(Department, Gender)
+  
+  # Crear gráfico de barras interactivo con plotly
+  plot <- plot_ly(salary_summary, 
+                  x = ~Department, 
+                  y = ~Average_Salary, 
+                  color = ~Gender, 
+                  type = 'bar', 
+                  barmode = 'group',
+                  text = ~paste("Department: ", Department, "<br>Gender: ", Gender, "<br>Average Salary: $", round(Average_Salary, 2)),
+                  hoverinfo = 'text') %>%
+    layout(title = 'Salario promedio mensual por género, posición y departamento',
+           xaxis = list(title = 'Departamento'),
+           yaxis = list(title = 'Salario Promedio (USD)'),
+           barmode = 'group')
+  
+  htmlwidgets::saveWidget(plot,"temp_plot.html")
+  browseURL("temp_plot.html")
+  
+}
+
+
+#* @apiTitle API para obtener la respuesta a la segunda Hipotesis
+
+#* ¿Cómo afecta el número de horas extras trabajadas al rendimiento (Job Rate) de los empleados en diferentes departamentos?
+#* @param file_path Ruta completa del archivo CSV
+#* @get /SHipo
+function(file_path) {
+  # Leer el archivo CSV
+  data <- read.csv(file_path)
+  
+  # Agrupar por departamento y calcular estadísticas resumen
+  dept_performance <- data %>%
+    group_by(Department) %>%
+    summarize(
+      avg_overtime = mean(OvertimeHours, na.rm = TRUE),
+      avg_job_rate = mean(JobRate, na.rm = TRUE)
+    )
+  
+  # Crear un gráfico de dispersión con plotly
+  fig <- plot_ly(
+    data = data,
+    x = ~OvertimeHours,
+    y = ~JobRate,
+    color = ~Department,
+    type = 'scatter',
+    mode = 'markers',
+    marker = list(size = 10),
+    text = ~paste("Departamento:", Department, "<br>Horas Extras:", OvertimeHours, "<br>Rendimiento:", JobRate)
+  ) %>%
+    layout(
+      title = "Relación entre Horas Extras y Rendimiento por Departamento",
+      xaxis = list(title = "Horas Extras"),
+      yaxis = list(title = "Rendimiento (Job Rate)"),
+      legend = list(title = "Departamento")
+    )
+  htmlwidgets::saveWidget(fig,"temp_plot.html")
+  browseURL("temp_plot.html")
+  
+}
+
+
+#* @apiTitle API para obtener la respuesta a la tercera Hipotesis
+
+#* ¿Cuál es el impacto del número de años de servicio en la probabilidad de que un empleado tome licencias no remuneradas?
+#* @param file_path Ruta completa del archivo CSV
+#* @get /THipo
+function(file_path) {
+  # Leer el archivo CSV
+  data <- read.csv(file_path)
+  
+  # Crear una variable binaria para indicar si el empleado tomó licencias no remuneradas
+  employess_data <- data %>%
+    mutate(Took_Unpaid_Leave = ifelse(UnpaidLeaves > 0, 1, 0))
+  
+  # Agrupar por años de servicio y calcular la tasa de empleados con licencias no remuneradas
+  years_leave_prob <- employess_data %>%
+    group_by(Years) %>%
+    summarize(
+      num_employees = n(),
+      num_with_leave = sum(Took_Unpaid_Leave),
+      leave_prob = num_with_leave / num_employees
+    )
+  
+  # Crear un gráfico de línea con plotly
+  fig <- plot_ly(
+    data = years_leave_prob,
+    x = ~Years,
+    y = ~leave_prob,
+    type = 'scatter',
+    mode = 'lines+markers',
+    marker = list(size = 8),
+    line = list(width = 2),
+    text = ~paste("Años de Servicio:", Years, "<br>Probabilidad de Licencia No Remunerada:", round(leave_prob, 2))
+  ) %>%
+    layout(
+      title = "Impacto de Años de Servicio en la Probabilidad de Tomar Licencias No Remuneradas",
+      xaxis = list(title = "Años de Servicio"),
+      yaxis = list(title = "Probabilidad de Licencia No Remunerada")
+    )
+  
+  htmlwidgets::saveWidget(fig,"temp_plot.html")
+  browseURL("temp_plot.html")
+  
+}
+
+
+
+
+
+#* @apiTitle API para obtener la respuesta a la cuarta Hipotesis
+
+#* ¿Los empleados en ciertos países  tienen más probabilidades de tomar licencias por enfermedad que en otros?
+#* @param file_path Ruta completa del archivo CSV
+#* @get /CHipo
+function(file_path) {
+  # Leer el archivo CSV
+  data <- read.csv(file_path)
+  
+  # Crear una variable binaria para indicar si el empleado tomó licencias por enfermedad
+  employee_data <- data %>%
+    mutate(Took_Sick_Leave = ifelse(SickLeaves > 0, 1, 0))
+  
+  # Agrupar por país y calcular la tasa de empleados con licencias por enfermedad
+  country_sick_leave_prob <- employee_data %>%
+    group_by(Country) %>%
+    summarize(
+      num_employees = n(),
+      num_with_sick_leave = sum(Took_Sick_Leave),
+      sick_leave_prob = num_with_sick_leave / num_employees
+    ) %>%
+    arrange(desc(sick_leave_prob))
+  
+  # Crear un gráfico de barras con plotly
+  fig_country <- plot_ly(
+    data = country_sick_leave_prob,
+    x = ~Country,
+    y = ~sick_leave_prob,
+    type = 'bar',
+    text = ~paste("País:", Country, "<br>Probabilidad de Licencia por Enfermedad:", round(sick_leave_prob, 2)),
+    marker = list(color = 'rgba(55, 128, 191, 0.7)', line = list(color = 'rgba(55, 128, 191, 1.0)', width = 1))
+  ) %>%
+    layout(
+      title = "Probabilidad de Licencias por Enfermedad por País",
+      xaxis = list(title = "País", tickangle = -45),
+      yaxis = list(title = "Probabilidad de Licencia por Enfermedad")
+    )
+  
+  htmlwidgets::saveWidget(fig_country,"temp_plot.html")
+  browseURL("temp_plot.html")
+  
+  
+}
+
+
+#* @apiTitle API para obtener la respuesta a la cuarta Hipotesis Segunda Parte
+
+#* ¿Los empleados en ciertos países  tienen más probabilidades de tomar licencias por enfermedad que en otros?
+#* @param file_path Ruta completa del archivo CSV
+#* @get /CHipo2
+function(file_path) {
+  # Leer el archivo CSV
+  data <- read.csv(file_path)
+  
+  # Agrupar por centro y calcular la tasa de empleados con licencias por enfermedad
+  center_sick_leave_prob <- data %>%
+    group_by(Center) %>%
+    summarize(
+      num_employees = n(),
+      num_with_sick_leave = sum(SickLeaves),
+      sick_leave_prob = num_with_sick_leave / num_employees
+    ) %>%
+    arrange(desc(sick_leave_prob))
+  
+  # Crear un gráfico de barras con plotly
+  fig_center <- plot_ly(
+    data = center_sick_leave_prob,
+    x = ~Center,
+    y = ~sick_leave_prob,
+    type = 'bar',
+    text = ~paste("Centro:", Center, "<br>Probabilidad de Licencia por Enfermedad:", round(sick_leave_prob, 2)),
+    marker = list(color = 'rgba(219, 64, 82, 0.7)', line = list(color = 'rgba(219, 64, 82, 1.0)', width = 1))
+  ) %>%
+    layout(
+      title = "Probabilidad de Licencias por Enfermedad por Centro",
+      xaxis = list(title = "Centro", tickangle = -45),
+      yaxis = list(title = "Probabilidad de Licencia por Enfermedad")
+    )
+  
+  
+  htmlwidgets::saveWidget(fig_center,"fig_center.html")
+  browseURL("fig_center.html")
+  
+}
+
+
+
+#* @apiTitle API para obtener la respuesta a la quinta Hipotesis
+
+#*¿Hay una relación entre el salario anual y la retención de empleados en diferentes departamentos?
+#* @param file_path Ruta completa del archivo CSV
+#* @get /QHipo
+function(file_path) {
+  # Leer el archivo CSV
+  data <- read.csv(file_path)
+  
+  # Agrupar por departamento y calcular la tasa de retención y salario anual promedio
+  dept_retention <- data %>%
+    group_by(Department) %>%
+    summarize(
+      num_employees = n(),
+      avg_annual_salary = mean(AnnualSalary, na.rm = TRUE),
+      avg_years = mean(Years, na.rm = TRUE)
+    ) %>%
+    mutate(
+      retention_rate = avg_years / max(avg_years)  # Normalizamos la tasa de retención
+    )
+  
+  # Crear un gráfico de dispersión con plotly para mostrar la relación entre salario y retención
+  fig <- plot_ly(
+    data = dept_retention,
+    x = ~avg_annual_salary,
+    y = ~retention_rate,
+    type = 'scatter',
+    mode = 'markers',
+    marker = list(size = 10),
+    text = ~paste("Departamento:", Department, "<br>Salario Anual Promedio:", round(avg_annual_salary, 2), "<br>Tasa de Retención:", round(retention_rate, 2)),
+    color = ~Department
+  ) %>%
+    layout(
+      title = "Relación entre Salario Anual y Tasa de Retención por Departamento",
+      xaxis = list(title = "Salario Anual Promedio (USD)"),
+      yaxis = list(title = "Tasa de Retención"),
+      legend = list(title = "Departamento")
+    )
+  
+  
+  htmlwidgets::saveWidget(fig,"temp_plot.html")
+  browseURL("temp_plot.html")
+  
+}
